471f29a5120d20e82e47e56dca12e4a9ff8c23b9
AnadelaJara <anadelajara@gmail.com>
2022-02-07 10:46:49 +0100

    4.3 ex 1-2

--- src/index.js
+++ src/index.js
@@ -8,6 +8,25 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+//endpoint getId
+
+server.get('/movie/:movieId', (req, res) => {
+  const requestParamsMovie = req.params.movieId;
+  const foundMovie = moviesFromJson.find(movie =>
+    movie.id === requestParamsMovie);
+  console.log(foundMovie.title);
+});
+
+// server.get("/posts/:slug", (req, res) => {
+//   const requestParamsSlug = req.params.slug;
+//   //buscarla información
+//   const postData = posts.find(post => post.slug === requestParamsSlug);
+//   postData.catList = postData.categories.split(",");
+//   console.log(postData);
+//   res.render("post", postData);
+// });
+
+
 //servidores de estáticos
 const staticServerPath = "./src/public-react";
 server.use(express.static(staticServerPath));
@@ -20,6 +39,7 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
 
 //endpoint get movies
 server.get("/movies", (req, res) => {
